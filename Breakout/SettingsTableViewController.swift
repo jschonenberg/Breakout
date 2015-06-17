4113a941c3e381c60f3acc696ecff91608193ba3
Jeroen Schonenberg <jeroen.schonenberg@gmail.com>
2015-06-17 23:19:44 +0200

    poc

--- Breakout/SettingsTableViewController.swift
+++ Breakout/SettingsTableViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SettingsTableViewController: UITableViewController {
+class SettingsTableViewController: UITableViewController, AVCaptureMetadataOutputObjectsDelegate{
 
     @IBOutlet weak var levelSegmentedControl: UISegmentedControl!
     
@@ -27,9 +28,12 @@ class SettingsTableViewController: UITableViewController {
     }
     
     @IBAction func levelChanged(sender: UISegmentedControl) {
-     Settings.level = Levels.levels[sender.selectedSegmentIndex]
-        
-        Settings.ResetRequired = true
+        if sender.selectedSegmentIndex == 3 {
+            captureQRCode()
+        } else {
+            Settings.level = Levels.levels[sender.selectedSegmentIndex]
+            Settings.ResetRequired = true
+        }
     }
     
     @IBAction func ballCountChanged(sender: UIStepper)
@@ -40,5 +44,54 @@ class SettingsTableViewController: UITableViewController {
     }
     
     @IBAction func ballSpeedModifierChanged(sender: UISlider) {
+    }
+    
+    let session = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    func captureQRCode() {
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        let input = AVCaptureDeviceInput.deviceInputWithDevice(device, error: nil) as! AVCaptureDeviceInput
+        session.addInput(input)
+        
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        session.addOutput(output)
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        let bounds = self.view.layer.bounds
+        self.previewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
+        self.previewLayer!.bounds = bounds
+        self.previewLayer!.position = CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds))
+        
+        self.view.layer.addSublayer(previewLayer)
+        session.startRunning()
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        for item in metadataObjects {
+            if let metadataObject = item as? AVMetadataMachineReadableCodeObject {
+                if metadataObject.type == AVMetadataObjectTypeQRCode {
+                    var QRstr = metadataObject.stringValue
+                    var rows = QRstr.componentsSeparatedByCharactersInSet(.whitespaceAndNewlineCharacterSet())
+                    
+                    var level: Array<[Int]> = Array<[Int]>()
+                    for row in rows {
+                        level.append( map(row) { String($0).toInt() ?? 0 } )
+                    }
+                    
+                    Settings.level = level
+                    Settings.ResetRequired = true
+                    
+                    
+                    
+                    // stop scanning
+                    session.stopRunning()
+                    previewLayer?.removeFromSuperlayer()
+                }
+            }
+        }
     }
 }
